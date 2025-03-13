6ee006cf2747b5a6b3f2970234a186d65e46a2d5
Mustafa Essam <zamalekzamalek62@yahoo.com>
2025-03-13 21:42:45 +0200

    fixing images

--- src/pages/Festival.jsx
+++ src/pages/Festival.jsx
@@ -6,12 +6,12 @@ const Festival = () => {
     return(
         <div className='empower-your-festival-container'>
                 <div className='slogans'>
-                    <ResponsiveTypography startFontSizeInPX={46} endFontSizeInPX={26} txt="Empower your festival with Rescounts’s all-in-one management platform" HTMLTag="h1" additionalClassNames='slogan-headline' />
-                    <ResponsiveTypography startFontSizeInPX={22} endFontSizeInPX={14} txt="Rescounts simplifies festival management with intuitive tools designed to enhance attendee experience and organizational efficiency." HTMLTag="h4" additionalClassNames='slogan-parag' />
+                    <ResponsiveTypography startFontSizeInPX={46} endFontSizeInPX={26} txt="Empower your festival with Rescounts' all-in-one management platform" HTMLTag="h1" additionalClassNames='slogan-headline' />
+                    <ResponsiveTypography startFontSizeInPX={22} endFontSizeInPX={14} txt="Rescounts simplifies festival management with intuitive tools designed to enhance attendee's experience and organizational efficiency." HTMLTag="h4" additionalClassNames='slogan-parag' />
                 </div>
 
                 <div className='img'>
-                    <img src={EmpowerYourFestivalImage} alt='Empower your festival with Rescounts’s all-in-one management platform' title="Empower your festival with Rescounts’s all-in-one management platform" />
+                    <img src={EmpowerYourFestivalImage} alt='Empower your festival with Rescounts all-in-one management platform' title="Empower your festival with Rescounts'’s' all-in-one management platform" />
                 </div>
         </div>
     )
